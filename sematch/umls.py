b94300abe6833f9956e2da0c815e1370c655c545
deepaknlp <deepak.gupta651@gmail.com>
2025-02-03 01:32:19 -0500

    Update umls.py

--- sematch/umls.py
+++ sematch/umls.py
@@ -2,15 +2,15 @@ from sematch.semantic.graph import UMLSDataTransform, Taxonomy
 from sematch.semantic.similarity import UMLSConceptSimilarity
 import pickle
 import os
-pickle_obj_path='/data/guptadk/Documents/CHiQA-Question-Understanding/data/KG/concept-obj-SNOMEDCT-US-MSH.pkl'
+pickle_obj_path=''
 if os.path.exists(pickle_obj_path):
     print("Loading concept object...")
     with open(pickle_obj_path, 'rb') as handle:
         concept = pickle.load(handle)
 else:
     concept = UMLSConceptSimilarity(Taxonomy(UMLSDataTransform(
-        path_to_MRCONSO_file='/data/guptadk/Documents/CHiQA-Question-Understanding/data/KG/2023AA/META/MRCONSO.RRF',
-        path_to_MRREL_file='/data/guptadk/Documents/CHiQA-Question-Understanding/data/KG/2023AA/META/MRREL.RRF'
+        path_to_MRCONSO_file='',
+        path_to_MRREL_file=''
         )))
     print("Saving concept object...")
     # Open a file and use dump()
@@ -20,4 +20,3 @@ else:
 
 print(concept.similarity('C0017601', 'C0232197', 'path'))
 
-# export PYTHONPATH=$PYTHONPATH:/data/guptadk/Documents/sematch
